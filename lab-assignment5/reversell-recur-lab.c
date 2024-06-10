89cfc72380554ce6b8719014fed6b55292a4931d
Anastasia Yakauleva <nastiyakovleva@gmail.com>
2024-06-10 12:35:12 -0700

    reversing the linked list using recursion

--- lab-assignment5/reversell-recur-lab.c
+++ lab-assignment5/reversell-recur-lab.c
@@ -14,16 +14,38 @@ typedef struct list{
 /*-----creating the nodes----------*/
 node_t* newNode(int num)
 {
+    node_t *newNode = (node_t*)malloc(sizeof(node_t));
+    if (newNode == NULL){
+        printf("Memory allocation failed");
+        exit(1);
+    }
+    newNode->data = num;
+    newNode->next = NULL;
+    return newNode;
  
 }
 /*---creating linked list----*/
 List* init_LL(){
+    List* list = (List*)malloc(sizeof(List));
+    if (list == NULL){
+        printf("Memory allocation failed");
+        exit(1);
+    }
+    list->head = NULL;
+    return list;
     
 }
 
 /*---Insert the nodes at the begining of the list---*/
 void insertFirst(List* l, int data){
-    
+    node_t* firstNode = newNode(data);
+    if (l->head == NULL){ //if our list is empty
+        l->head = firstNode;
+        return;
+    }
+    firstNode->next = l->head;
+    l->head = firstNode;
+    return;
 
 }
 
@@ -43,12 +65,22 @@ void display(List* l)
 /*-------reversing the linked list using recursion------*/
 void reverse(List* l, node_t* ptr)
 {
-    
-    
-    
-    
-//insert your code here
-    
+    // if the list is empty, return without doing anything.
+    if(l->head == NULL) {
+        return;
+    }
+
+    // Base case: if it is the last node
+    if(ptr->next == NULL) {
+        l->head = ptr; // the last node of the orig lst becomes the head
+        return;
+    }
+
+    // Recursive case
+    reverse(l, ptr->next);
+
+    ptr->next->next = ptr; // loop pointers of current and next el
+    ptr->next = NULL; // beak pointers loop and set current node point to 0
     
 }
 
