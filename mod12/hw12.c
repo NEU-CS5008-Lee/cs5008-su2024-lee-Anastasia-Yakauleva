60d2f4f1f7e500fc80df720cbccc9086dc3e5ab4
Anastasia Yakauleva <nastiyakovleva@gmail.com>
2024-08-01 17:12:58 -0700

    Concurrency

---
+++ mod12/hw12.c
@@ -0,0 +1,82 @@
+// name: <your name here>
+// email: <your email here>
+// Compile with:
+//
+// gcc -lpthread hw12.c -o hw12
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h> // provides pthread_mutex_t data type
+
+#define NTHREADS 100
+
+// shared variable
+int counter = 0;
+
+pthread_mutex_t lock;
+
+// thread to be executed - unspecified variable arguments
+void* thread1 (void* vargp) {
+    pthread_mutex_lock(&lock);
+    counter = counter + 1;
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
+void* thread2 (void* vargp) {
+    pthread_mutex_lock(&lock);
+    // add 5 to counter
+    counter = counter + 5;
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
+void* thread3 (void* vargp) {
+    pthread_mutex_lock(&lock);
+    // subtract 2 from counter
+    counter = counter - 2;
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
+void* thread4 (void* vargp) {
+    pthread_mutex_lock(&lock);
+    // subtract 10 from counter
+    counter = counter - 10;
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
+int main() {
+    // array to keep Pthread IDs of created threads
+    pthread_t tid[NTHREADS*4];
+    int i;
+
+    // Initialize the mutex
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
+
+    printf("Counter starts at %d\n", counter);
+
+    // create and run the threads
+    for (i = 0; i < NTHREADS; ++i) {
+        pthread_create(&(tid[i]), NULL, thread1, NULL);
+        pthread_create(&(tid[i + NTHREADS]), NULL, thread2, NULL);
+        pthread_create(&(tid[i + 2 * NTHREADS]), NULL, thread3, NULL);
+        pthread_create(&(tid[i + 3 * NTHREADS]), NULL, thread4, NULL);
+    }
+
+    // wait until all threads are done
+    for (i = 0; i < NTHREADS*4; ++i){
+        pthread_join(tid[i], NULL);
+    }
+
+    printf("Counter ends at %d\n", counter);
+
+    // Destroy the mutex object
+    pthread_mutex_destroy(&lock);
+
+    return 0;
+}
---
+++ mod12/philosopher-lab.c
@@ -0,0 +1,67 @@
+// name: Anastasia Yakauleva
+// email: yakauleva.a@northeastern.edu
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+pthread_mutex_t chopstick[5];//lock variable
+
+// Thread to be executed
+void *philosopher(void *x)
+{
+    //Treat variable x as a pointer to an int and then extract the value into n
+    int* a=(int*)x;
+    int n=*a;
+
+    while (1) {
+        while (1) {
+        printf("Philosopher %d is thinking\n", n);
+        sleep(1);
+
+        // Take the left chopstick
+        pthread_mutex_lock(&chopstick[n]);
+        // Take the right chopstick
+        pthread_mutex_lock(&chopstick[(n + 1) % 5]);
+
+        printf("Philosopher %d is eating using chopstick[%d] and chopstick[%d]\n", n, n, (n + 1) % 5);
+        sleep(1);
+        printf("Philosopher %d finished eating\n", n);
+
+        // Put down the left chopstick
+        pthread_mutex_unlock(&chopstick[n]);
+        // Put down the right chopstick
+        pthread_mutex_unlock(&chopstick[(n + 1) % 5]);
+        }
+    }
+
+
+}
+
+
+/*------------ Main Program---------*/
+int main()
+{
+    int i,val[5];
+    pthread_t threads[5];
+    //initializing the mutex (for chopsticks)
+    for(i=0;i<5;i++){
+        pthread_mutex_init(&chopstick[i],NULL);
+    }
+
+    //create and run the thread
+    for(i=0;i<5;i++){
+        val[i]=i;
+        pthread_create(&threads[i],NULL,(void *)philosopher,&val[i]);
+    }
+
+    //wait until all the threads are done
+    for(i=0;i<5;i++){
+        pthread_join(threads[i],NULL);
+    }
+
+    // Destroying the mutex
+    for(i=0;i<5;i++){
+        pthread_mutex_destroy(&chopstick[i]);
+    }
+
+    return 0;
+}
